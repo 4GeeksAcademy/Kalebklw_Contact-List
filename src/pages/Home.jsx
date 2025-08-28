7d6235fe6e21733cca522450de7690e6274642ff
Kaleb Williams <kalebklw@gmail.com>
2025-08-28 01:06:28 +0000

    working on functionality of API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,84 @@
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { useState, useEffect } from "react";
+import {Link} from "react-router-dom";
 
 export const Home = () => {
 
+const [user, setUser] = useState("Kaleb") 
+
   const {store, dispatch} =useGlobalReducer()
+  const createAgenda = () =>{
+	let options = {
+		method: "POST",
+		headers: {"content-type":"application/json"},
+		body: JSON.stringify({
+			"slug": "Kalebklw",
+  			"id": 0
+		})
+	}
+	fetch(store.baseUrl + "agendas/Kalebklw", options)
+	.then((resp) => resp.json())
+	.then((data) =>{ 
+		setUser(data.detail)
+		console.log("Tag For Agenda's Data: ", data)})
+  }
+  
+const createContacts = () =>{
+	const options = {
+		method: "POST",
+		headers: {"content-type":"application/json"},
+		body: JSON.stringify({
+			"name": "user3",
+            "phone": "phone3",
+            "email": "email3",
+            "address": "address3"
+		})
+	}
+	fetch(store.baseUrl +"agendas/Kalebklw/contacts", options)
+	.then((resp) => resp.json())
+	.then((data) => console.log("Data of Contacts: ", data))
+}
+
+	useEffect(
+		() =>{
+			createAgenda()
+		}, []
+	)
 
 	return (
+
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+			<h1>Hello!!</h1>
+			{user}
+			<Link to = "/test">
+			Go To Test Page
+			</Link>
+
+			<button onClick={()=> {
+				createContacts()
+			}}>
+				Click to Add Contact
+			</button>
+
+			<div className="m-3">
+				<button onClick={() => {
+					dispatch({
+						type: "set-fName", 
+						payload: "Alex"
+					})
+				}}> {store.fName} </button>
+			</div>
+
+			<div>
+				<button onClick={() => {
+					dispatch({
+						type: "set-lName",
+						payload: "Ayala"
+					})
+				}}> {store.lName} </button>
+			</div>
+
 		</div>
 	);
 }; 
--- src/store.js
+++ src/store.js
@@ -1,32 +1,35 @@
 export const initialStore=()=>{
   return{
-    message: null,
-    todos: [
-      {
-        id: 1,
-        title: "Make the bed",
-        background: null,
-      },
-      {
-        id: 2,
-        title: "Do my homework",
-        background: null,
-      }
-    ]
+    fName: "Kaleb",
+    lName: "Williams",
+    baseUrl: "https://playground.4geeks.com/contact/"
   }
 }
 
-export default function storeReducer(store, action = {}) {
-  switch(action.type){
-    case 'add_task':
+export default function storeReducer(store, action = {}){
 
-      const { id,  color } = action.payload
-
-      return {
-        ...store,
-        todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
-      };
-    default:
-      throw Error('Unknown action.');
-  }    
+  if(action.type == "set-fName"){
+    return {
+      ...store, fName: action.payload
+  }
 }
+
+  if(action.type == "set-lName"){
+    return {
+      ...store, lName: action.payload
+    }
+  }
+
+}
+  // switch(action.type){
+  //   case 'add_task':
+
+  //     const { id,  color } = action.payload
+
+  //     return {
+  //       ...store,
+  //       todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
+  //     };
+  //   default:
+  //     throw Error('Unknown action.');
+  // }    
